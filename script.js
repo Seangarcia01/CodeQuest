f7c8197daf9a37cf0dd25f338845e38f901f341f
Saru <136166825+Seangarcia01@users.noreply.github.com>
2025-08-02 11:09:09 +0000

    script.js

--- script.js
+++ script.js
@@ -16,13 +16,15 @@ const imageContainer = document.getElementById('image-container');
 
 const imagePaths = [
   "assets/1.jpg",
+  "assets/2.jpg",
   "assets/3.jpg",
   "assets/4.jpg",
   "assets/5.jpg",
   "assets/6.jpg",
   "assets/7.jpg",
   "assets/8.jpg",
-  "assets/9.jpg"
+  "assets/9.jpg",
+  "assets/calle2.jpg",
 ];
 
 // Spawn floating image
